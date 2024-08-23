36c47739a09676f48f1b0360f0b2be5b0383d5d8
mikio（岡村美紀） <>
2024-08-23 18:39:25 +0900

    fix: 🐛 resize canvas size to window size

--- 7-treasure/sketch.js
+++ 7-treasure/sketch.js
@@ -2,22 +2,21 @@ let tileWidth;
 let tileNum = 10;
 let r;
 
-
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(windowWidth, windowHeight);
 
   tileWidth = width / tileNum;
 }
 
 function draw() {
   background(220);
-  for (let x=0; x<width+tileWidth; x+= tileWidth){
-     for (let y=0; y<width+tileWidth; y+= tileWidth){
-       r = mouseY/3;
-       noFill();
-       strokeWeight(map(mouseX,0,width,0,15));
-       stroke(100);
-       ellipse(x, y, r);
-     }
+  for (let x = 0; x < width + tileWidth; x += tileWidth) {
+    for (let y = 0; y < height + tileWidth; y += tileWidth) {
+      r = mouseY / 3;
+      noFill();
+      strokeWeight(map(mouseX, 0, width, 0, 15));
+      stroke(100);
+      ellipse(x, y, r);
+    }
   }
 }
